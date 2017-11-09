3226244dc1efd8ffa494a185021c70e849a45523
Olivier Anguenot <olivier.anguenot@al-enterprise.com>
2017-11-09 14:34:25 +0100

    FIrst version

---
+++ app/components/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="content">
+        <h5>Time to <a href="https://hub.openrainbow.com/">Play with SDK and APIs</a>.</h5>
+        <p>Version <span> {{ version }} </span></p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            version: rainbowSDK.version()
+        }
+    }
+}
+</script>
+
