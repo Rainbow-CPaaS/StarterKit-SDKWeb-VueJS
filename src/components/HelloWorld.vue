644d0be7b0d8f3b178cfc3f0017157c62558b64e
Konrad Hyzy <konrad.hyzy@al-enterprise.com>
2020-07-07 18:06:31 +0200

    Update vueJS example - include initialization and connection

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,8 +6,36 @@
 </template>
 
 <script>
-import rainbowSDK from 'rainbow-web-sdk'
+import rainbowSDK from 'rainbow-web-sdk';
 
+
+var onReady = function onReady() {
+    console.log('[Hello World] :: On SDK Ready !');
+	rainbowSDK.connection.signin('loginEmail', 'password').then(user => {
+		return user
+	}).catch(err => { 
+		console.log(err) 
+	})
+};
+
+var onLoaded = function onLoaded() {
+    console.log('[Hello World] :: On SDK Loaded !');
+
+    rainbowSDK
+        .initialize('appId', 'appSecret')
+        .then(() => {
+            console.log('[Hello World] :: Rainbow SDK is initialized!');
+        })
+        .catch(err => {
+            console.log('[Hello World] :: Something went wrong with the SDK.', err);
+        });
+};
+/* Listen to the SDK event RAINBOW_ONREADY */
+document.addEventListener(rainbowSDK.RAINBOW_ONREADY, onReady)
+/* Listen to the SDK event RAINBOW_ONLOADED */
+document.addEventListener(rainbowSDK.RAINBOW_ONLOADED, onLoaded)
+/* Load the SDK */
+rainbowSDK.load();
 
 export default {
   name: "HelloWorld",
